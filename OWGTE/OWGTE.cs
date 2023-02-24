563a76d535fae7f07dca5d8da53fe5da2c6fd55f
BlenderAddictd <90448653+BlenderAddictd@users.noreply.github.com>
2023-02-24 14:08:28 +0100

    Add files via upload

---
+++ OWGTE/OWGTE.cs
@@ -0,0 +1,15 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OWGTE
+{
+    public class OWGTE : ModBehaviour
+    {
+        public static OWGTE Instance;
+        private void Start()
+        {
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "OWGTE", "assets/Translation.xml");
+        }
+    }
+}
